1349a354b7311704b9c113a0e47b26aae41497b8
hailuser90-byte <hailuser90@gmail.com>
2026-07-02 14:48:32 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	host: 'Wanasa.aternos.me',
+    port: 64433,
+    username: 'Jwno',
+    password: 'qhsnx73h',
+    version: "26.1"   
+	// Use mojang for yggdrasil login, microsoft for Microsoft login
+	auth: 'mojang'
+};
+
